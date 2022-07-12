830b9359b982099b8afc82ca97844c81f1f29b54
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-12 12:45:20 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,36 @@
+Variable "allocated_storage" {
+  type = string
+}
+Variable "engine" {
+  type = string
+}
+Variable "engine_version" {
+  type = string
+}
+Variable "license_model" {
+  type = string
+}
+Variable "instance_class" {
+  type = string
+}
+Variable "db_subnet_group_name" {
+  type = string
+}
+Variable "db_name" {
+  type = string
+}
+Variable "username" {
+  type = string
+}
+Variable "password" {
+  type = string
+}
+Variable "port" {
+  type = string
+}
+Variable "skip_final_snapshot" {
+  type = string
+}
+Variable "multi_az" {
+  type = string
+}
